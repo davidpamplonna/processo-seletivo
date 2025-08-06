81790e61874be357714a26e65b603a5515dcba4d
David Pamplona <davidpamplonna@gmail.com>
2025-08-06 10:14:27 -0300

    aplicando logica de selecao

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,49 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 
-
     public static void main (String[] args){
-        System.out.println("Processo Seletivo");
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos(){
+      String[] candidatos = {"Felipe", "Marcia","Julia", "Paulo"};
+
+      int candidatosSelecionados = 0;
+      int candidatoAtual = 0;
+      double salarioBase = 2000.0;
+
+      while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+          String candidato = candidatos[candidatoAtual];
+          double salarioPretendido = ValorPretendido();
+
+          System.out.println("O candidato " + candidato + " Solicitou este valor de sálario " + salarioPretendido);
+
+          if (salarioBase >= salarioPretendido){
+              System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+              candidatosSelecionados++;
+          }
+          candidatoAtual++;
+      }
+    }
+
+    static double ValorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
+    static void analisarCanditdato(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o condidato");
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("Aguardando contra proposta");
+        }
+
+        else{
+            System.out.println("Aguardando resultado dos demais candidatos");
+        }
     }
 }
